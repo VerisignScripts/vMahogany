f15ad11639a8fc8f72ef09a9f4c294073249c2b2
Aidan O'Connell <Verisign@outlook.com>
2013-05-29 22:19:20 +0100

    Test

---
+++ org/aidan/scripts/Mahogany/util/Methods.java
@@ -0,0 +1,11 @@
+package org.aidan.scripts.jungleSpiders.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aid
+ * Date: 29/05/13
+ * Time: 21:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Methods {         // self explanatory
+}
---
+++ org/aidan/scripts/Mahogany/util/Variables.java
@@ -0,0 +1,14 @@
+package org.aidan.scripts.jungleSpiders.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aid
+ * Date: 29/05/13
+ * Time: 21:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Variables {     // self explanatory
+    // every variable in this or the Methods class needs to be public
+    // so that other classes can access it
+    // if its not public you'll get an error
+}
