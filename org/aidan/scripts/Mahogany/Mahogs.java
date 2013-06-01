4e390d191ef67a5115bccf3ad5e5def037910dec
Aidan O'Connell <Verisign@outlook.com>
2013-06-01 21:22:07 +0100

    fixed chopping

--- org/aidan/scripts/Mahogany/Mahogs.java
+++ org/aidan/scripts/Mahogany/Mahogs.java
@@ -16,8 +16,10 @@ import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.client.Client;
 
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
@@ -97,14 +99,40 @@ public class Mahogs extends ActiveScript implements PaintListener, MessageListen
         }
     }
 
+    private Image getImage(String url) {
+        try {
+            return ImageIO.read(new URL(url));
+        } catch(IOException e) {
+            return null;
+        }
+    }
+
+    private final Color color1 = new Color(102, 102, 102);
+    private final Color color2 = new Color(255, 255, 255);
+    private final Color color3 = new Color(255, 255, 0);
+
+    private final BasicStroke stroke1 = new BasicStroke(1);
+
+    private final Font font1 = new Font("Arial", 0, 11);
+
+    private final Image img1 = getImage("http://images3.wikia.nocookie.net/__cb20100913144807/runescape/nl/images/d/d3/Woodcutting_cape.png");
+
     public void onRepaint(Graphics g1) {
         Graphics2D g = (Graphics2D)g1;
         int profit = logPrice * logsChopped;
-
-        g.drawString("Profit: " + profit, 349, 330);
-        g.drawString("Runtime: " + runTime.toElapsedString(), 349, 239);
-        g.drawString("Logs Chopped: " + logsChopped, 349, 271);
-        g.drawString("Status: " + s, 349, 307);
+        g.setColor(color1);
+        g.setStroke(stroke1);
+        g.fillRect(299, 306, 213, 29);
+        g.fillRect(376, 214, 139, 74);
+        g.setColor(color2);
+        g.setStroke(stroke1);
+        g.drawRect(376, 214, 139, 74);
+        g.setFont(font1);
+        g.setColor(color3);
+        g.drawString("Runtime: " + runTime.toElapsedString(), 386, 238);
+        g.drawString("Logs: " + logsChopped, 386, 252);
+        g.drawString("Profit: " + profit, 386, 266);
+        g.drawString("Status: " + s, 302, 327);
+        g.drawImage(img1, 284, 203, null);
     }
-
 }
--- org/aidan/scripts/Mahogany/nodes/Chopping.java
+++ org/aidan/scripts/Mahogany/nodes/Chopping.java
@@ -2,11 +2,13 @@ package org.aidan.scripts.Mahogany.nodes;
 
 import org.aidan.scripts.Mahogany.Mahogs;
 import org.aidan.scripts.Mahogany.util.Variables;
+import org.powerbot.core.script.job.Task;
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.methods.tab.Inventory;
 import org.powerbot.game.api.methods.widget.Camera;
+import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 
 
@@ -15,7 +17,8 @@ public class Chopping extends Node {
 
     @Override
     public boolean activate() {
-        return !Inventory.isFull() && Inventory.contains(Variables.sticks) && Variables.treeArea.contains(Players.getLocal());
+        SceneObject theTree = SceneEntities.getNearest(Variables.tree);
+        return theTree != null && !Inventory.isFull() && Inventory.contains(Variables.sticks) && Variables.treeArea.contains(Players.getLocal());
     }
 
     @Override
@@ -30,6 +33,12 @@ public class Chopping extends Node {
                 }
             } else {
                 Camera.turnTo(theTree);
+            }
+        } else {
+            Timer t = new Timer( 3200);
+            t.reset();
+            while (t.isRunning() && theTree == null){
+                Task.sleep(300, 500);
             }
         }
     }
